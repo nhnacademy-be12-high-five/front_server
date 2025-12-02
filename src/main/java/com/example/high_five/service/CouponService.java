9cc892f22880eb0aef908c30500c9ef3011f7698
Poly-Etilen <wlsdud04060@naver.com>
2025-12-02 12:19:58 +0900

    fix : gateway url 수정

--- src/main/java/com/example/high_five/service/CouponService.java
+++ src/main/java/com/example/high_five/service/CouponService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@FeignClient(name = "GATEWAY")
+@FeignClient(name = "GATEWAY", url = "http://gateway-server:8000")
 public interface CouponService {
 
     @GetMapping("/api/coupons/members/{memberId}")
