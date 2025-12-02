1bb3c4c34c7bfcb41c9092093bb0100ebe9587da
Poly-Etilen <wlsdud04060@naver.com>
2025-12-02 16:51:24 +0900

    fix : CouponService url 수정

--- src/main/java/com/example/high_five/service/CouponService.java
+++ src/main/java/com/example/high_five/service/CouponService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@FeignClient(name = "GATEWAY", url = "http://localhost:8082")
+@FeignClient(name = "GATEWAY", url = "http://gateway-server:8000")
 public interface CouponService {
 
     @GetMapping("/api/coupons/members/{memberId}")
