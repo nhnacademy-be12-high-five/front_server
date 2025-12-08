7d7ae9d8c5966fbc9d9ae364726a0a54920a3e29
Poly-Etilen <wlsdud04060@naver.com>
2025-12-08 17:47:59 +0900

    ✅test : 특정 도서 쿠폰 정책 상세 조회 페이지 수정

--- src/main/java/com/example/high_five/dto/coupon/CouponPolicyRequestDto.java
+++ src/main/java/com/example/high_five/dto/coupon/CouponPolicyRequestDto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,4 +18,6 @@ public class CouponPolicyRequestDto {
     private Long discountValue;
     private Long minOrderValue;
     private Long maxDiscountValue;
+    private List<Long> targetCategoryIds;
+    private List<Long> targetBookIds;
 }
