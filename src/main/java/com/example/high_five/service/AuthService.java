c2037c1ea90bc47093638ec287a3216fed8fc901
TAEHUN <fbxogns321@naver.com>
2025-12-09 16:07:51 +0900

    :memo: front-practice pull 받음

--- src/main/java/com/example/high_five/service/AuthService.java
+++ src/main/java/com/example/high_five/service/AuthService.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "gateway-server", contextId = "authClient", url = "${gateway.uri}")
+@FeignClient(name = "gateway-server", contextId = "authClient", url = "localhost:8000")
 public interface AuthService {
 
     @PostMapping("/api/auth/login")
