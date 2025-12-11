7819dea2ead75317371f3dcc979020252a2d22af
yunaa00 <0305lily@naver.com>
2025-12-11 18:10:27 +0900

    category update

--- src/main/java/com/example/high_five/controller/book/BookController.java
+++ src/main/java/com/example/high_five/controller/book/BookController.java
@@ -17,7 +17,6 @@ public class BookController {
     /**
      * 도서 상세 화면
      */
-
     @GetMapping("/books/{book-id}")
     public String getBookDetail(@PathVariable("book-id") Long id, Model model) {
 
