797eddeade2d0c7aecbc50e881ad8e12dede7714
TAEHUN <fbxogns321@naver.com>
2025-12-13 15:00:18 +0900

    :ambulance: 장바구니 DELETE, PUT 전부 POST로 변경(다시 롤백)

--- src/main/java/com/example/high_five/service/CartService.java
+++ src/main/java/com/example/high_five/service/CartService.java
@@ -25,20 +25,20 @@ public interface CartService {
             @SpringQueryMap Pageable pageable
     );
 
-    @PostMapping("/api/cart/items")
+    @DeleteMapping("/api/cart/items")
     ResponseEntity<Void> deleteAllCartItem(
             @RequestHeader(name = "X-USER-ID", required = false) Long memberId,
             @RequestHeader(name = "X-GUEST-ID", required = false) String guestId
     );
 
-    @PostMapping("/api/cart/items")
+    @PutMapping("/api/cart/items")
     ResponseEntity<CartUpdateResponse> updateQuantity(
             @RequestBody CartItemUpdateRequest request,
             @RequestHeader(name = "X-USER-ID", required = false) Long memberId,
             @RequestHeader(name = "X-GUEST-ID", required = false) String guestId
     );
 
-    @PostMapping("/api/cart/items/{bookId}")
+    @DeleteMapping("/api/cart/items/{bookId}")
     ResponseEntity<Void> deleteOneItem(
             @PathVariable("bookId") Long bookId,
             @RequestHeader(name = "X-USER-ID", required = false) Long memberId,
@@ -52,7 +52,7 @@ public interface CartService {
     );
 
     // [추가] 비회원 장바구니 삭제 요청 (백엔드로)
-    @PostMapping("/api/cart/guest")
+    @DeleteMapping("/api/cart/guest")
     ResponseEntity<Void> deleteGuestCartOnly(
             @RequestHeader(name = "X-USER-ID", required = false) Long memberId,
             @RequestHeader(name = "X-GUEST-ID", required = false) String guestId
